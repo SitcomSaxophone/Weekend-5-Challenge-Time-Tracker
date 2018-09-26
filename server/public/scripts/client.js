38dc477faba6d3252ad000dbb366fe51516fcfad
Samuel Solberg <pauliesays65@gmail.com>
2018-09-26 16:07:14 -0500

    controllers and views folders set

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,16 @@
-const app = angular.module('TimeApp', ['$ngroute']);
+const app = angular.module('TimeApp', ['ngroute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'views/entry.html',
+        // controller: 'EntryController as vm'
+    })
+    .when('/projects', {
+        templateUrl: 'views/projects.html',
+        // controller: 'ProjectsController as vm'
+    })
+    .otherwise({
+        template: '<h1>404</h1>'
+    });
+}]);
