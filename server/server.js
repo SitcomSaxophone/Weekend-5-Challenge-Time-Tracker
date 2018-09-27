a59b19906b58d2cfc03cee941c087c9ef13eff8a
Samuel Solberg <pauliesays65@gmail.com>
2018-09-27 10:16:03 -0500

    pool.js created

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ const app = express();
 
 // Uses
 app.use(express.static('server/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); // AngularJS
+app.use(bodyParser.urlencoded({extended: true})); // JQuery
 
 // Spin up server
 const port = process.env.PORT || 5000;
