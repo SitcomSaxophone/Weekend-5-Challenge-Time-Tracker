c300c023dbe4238f47ec1f5eba19850bec30a834
Samuel Solberg <pauliesays65@gmail.com>
2018-09-27 11:24:58 -0500

    server side POST route written | entry input ng-models added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Require
 const express = require('express');
 const bodyParser = require('body-parser');
+const pool = require('./modules/pool');
 
 // Globals
 const app = express();
@@ -9,6 +10,20 @@ const app = express();
 app.use(express.static('server/public'));
 app.use(bodyParser.json()); // AngularJS
 app.use(bodyParser.urlencoded({extended: true})); // JQuery
+
+app.post('/', (req, res) => {
+    let entry = req.body;
+    pool.query(`INSERT INTO "entry"("description", "start_time", "end_time", "project_id")
+                VALUES($1, timestamp '$2 $3', timestamp '$2 $4', $5);`,
+                [entry.description, entry.date, entry.start_time, entry.end_time, project_id])
+                .then(() => {
+                    res.sendStatus(201);
+                })
+                .catch((error) => {
+                    console.log('Error making POST: ', error);
+                    res.sendStatus(500);
+                });
+});
 
 // Spin up server
 const port = process.env.PORT || 5000;
