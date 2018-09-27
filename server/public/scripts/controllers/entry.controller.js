e84038ad19663924a5b52d2b8128be79d93410ea
Samuel Solberg <pauliesays65@gmail.com>
2018-09-27 14:01:11 -0500

    client side entry GET and POST written

--- server/public/scripts/controllers/entry.controller.js
+++ server/public/scripts/controllers/entry.controller.js
@@ -1,5 +1,32 @@
 app.controller('EntryController', ['$http', function ($http) {
     let vm = this;
 
+    vm.addNewEntry = function (entryToAdd) {
+        $http({
+            method: 'POST',
+            url: '/entries',
+            data: entryToAdd
+        })
+        .then(function () {
+            vm.getEntries();
+        })
+        .catch(function (error) {
+            alert('Error making POST: ', error)
+        });
+    };
 
+    vm.getEntries = function () {
+        $http({
+            method: 'GET',
+            url: '/entries'
+        })
+        .then(function (response) {
+            vm.history = response.data;
+        })
+        .catch(function (error) {
+            alert('Error making GET: ', error);
+        });
+    };
+
+    vm.getEntries();
 }]);
