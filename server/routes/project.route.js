cc3c55b106fea2b74181ae68161e63b29917a805
Samuel Solberg <pauliesays65@gmail.com>
2018-09-28 11:12:56 -0500

    server side projects GET request

--- server/routes/project.route.js
+++ server/routes/project.route.js
@@ -8,7 +8,7 @@ router.post('/', (req, res) => {
     let entry = req.body;
     pool.query(`INSERT INTO "project"("name")
                 VALUES($1);`,
-        [entry.name])
+                [entry.name])
         .then(() => {
             res.sendStatus(201);
         })
@@ -16,6 +16,17 @@ router.post('/', (req, res) => {
             console.log('Error making POST: ', error);
             res.sendStatus(500);
         });
+});
+
+router.get('/', (req, res) => {
+    pool.query(`SELECT * FROM "project";`)
+    .then((results) => {
+        res.send(results.rows);
+    })
+    .catch((error) => {
+        console.log('Error making GET: ', error);
+        res.sendStatus(500);
+    });
 });
 
 //Exports
