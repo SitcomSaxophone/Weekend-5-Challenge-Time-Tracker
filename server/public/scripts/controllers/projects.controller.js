8655d8cecfadedc0ccde7d22474ed8348783fe21
Samuel Solberg <pauliesays65@gmail.com>
2018-09-28 10:55:01 -0500

    client side projects GET request

--- server/public/scripts/controllers/projects.controller.js
+++ server/public/scripts/controllers/projects.controller.js
@@ -1,5 +1,18 @@
 app.controller('ProjectsController', ['$http', function ($http) {
     let vm = this;
 
+    vm.getProjects = function () {
+        $http({
+            method: 'GET',
+            url: '/projects'
+        })
+        .then(function (response) {
+            vm.projects = response.data;
+        })
+        .catch(function (error) {
+            alert('Error making GET: ', error);
+        });
+    };
 
+    vm.getProjects();
 }]);
