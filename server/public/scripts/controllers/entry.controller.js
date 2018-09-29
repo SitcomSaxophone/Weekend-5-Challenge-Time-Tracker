2810c0a9a4950d2e460d9c77f79a384411306557
Samuel Solberg <pauliesays65@gmail.com>
2018-09-29 15:37:45 -0500

    entry duration working thanks to Eli and Daniel

--- server/public/scripts/controllers/entry.controller.js
+++ server/public/scripts/controllers/entry.controller.js
@@ -9,12 +9,12 @@ app.controller('EntryController', ['$http', function ($http) {
             url: '/entries',
             data: vm.newEntry
         })
-        .then(function () {
-            vm.getEntries();
-        })
-        .catch(function (error) {
-            alert('Error making POST: ', error)
-        });
+            .then(function () {
+                vm.getEntries();
+            })
+            .catch(function (error) {
+                alert('Error making POST: ', error)
+            });
     }; // end addNewEntry
 
     vm.getEntries = function () {
@@ -22,16 +22,23 @@ app.controller('EntryController', ['$http', function ($http) {
             method: 'GET',
             url: '/entries'
         })
-        .then(function (response) {
-            vm.history = response.data;
-            for (let value of vm.history) {
-                vm.entryDate = moment(value.date).format('MM/DD/YYYY');
-            };
-            vm.newEntry = {};
-        })
-        .catch(function (error) {
-            alert('Error making GET: ', error);
-        });
+            .then(function (response) {
+                vm.history = response.data;
+                for (let value of vm.history) {
+                    vm.entryDate = moment(value.date).format('MM/DD/YYYY');
+                    let st = moment(value.start_time)._i;
+                    let et = moment(value.end_time)._i;
+                    let stSplit = st.split(":");
+                    let etSplit = et.split(":");
+                    value.difference = (((etSplit[0]*60 + Number(etSplit[1]) + etSplit[2]/60) - (stSplit[0]*60 + Number(stSplit[1]) + stSplit[2]/60))/60).toFixed(2);
+                    console.log(value.difference);
+                };
+                // let d = Integer.parsInt(c[0]) + Integer.parseInt(c[1]) / 60.0;
+                vm.newEntry = {};
+            })
+            .catch(function (error) {
+                alert('Error making GET: ', error);
+            });
     }; // end getEntries
 
     vm.getProjects = function () {
@@ -39,15 +46,15 @@ app.controller('EntryController', ['$http', function ($http) {
             method: 'GET',
             url: '/projects'
         })
-        .then(function (response) {
-            for (let i = 0; i < response.data.length; i++) {
-                parseInt(response.data[i].id);      
-            };
-            vm.projects = response.data;
-        })
-        .catch(function (error) {
-            alert('Error making GET: ', error);
-        });
+            .then(function (response) {
+                for (let i = 0; i < response.data.length; i++) {
+                    parseInt(response.data[i].id);
+                };
+                vm.projects = response.data;
+            })
+            .catch(function (error) {
+                alert('Error making GET: ', error);
+            });
     }; // end getProjects
 
     vm.getProjects();
@@ -58,11 +65,11 @@ app.controller('EntryController', ['$http', function ($http) {
             method: 'DELETE',
             url: `/entries/${data.id}`
         })
-        .then(function () {
-            vm.getEntries();
-        })
-        .catch(function (error) {
-            alert('Error making DELETE: ', error);
-        });
+            .then(function () {
+                vm.getEntries();
+            })
+            .catch(function (error) {
+                alert('Error making DELETE: ', error);
+            });
     }; // end deleteEntry
 }]);
